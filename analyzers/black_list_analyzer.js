1668c5c54e0b041e89f9a81009f375699c5350bd
Boomerang <boomerang@trafficstars.com>
2018-03-14 14:18:44 +0200

    added mock for black list analyze

---
+++ analyzers/black_list_analyzer.js
@@ -0,0 +1,9 @@
+//
+/** function for chacking if webpage is in some arbitrary black list
+ * It is just a mock to show that we actually can check it
+ */
+async function isInBlackList(url) {
+    return false;
+}
+
+exports.isInBlackList = isInBlackList;
